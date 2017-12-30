8247617b724fbdc909b872c5893a4259d4d12dca
Kuncoro Atmojo <kuncoro@Kuncoros-MacBook-Pro.local>
2017-12-30 13:44:54 +0700

    add product unit test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:product) { FactoryGirl.build  :product }
+  subject { product }
+  
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :price }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+  it { should validate_presence_of :quantity }
 end
